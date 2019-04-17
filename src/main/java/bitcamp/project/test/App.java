2a794ecb5e8a518bb7e6ce1273b3646eb08dc207
meeyoungchoi <goodlife1359@gmail.com>
2019-04-17 17:00:53 +0900

    .

--- src/main/java/bitcamp/project/test/App.java
+++ src/main/java/bitcamp/project/test/App.java
@@ -1,30 +1,33 @@
 package bitcamp.project.test;
 
 import bitcamp.project.test.service.LessonService;
+import bitcamp.project.test.service.MemberService;
 import bitcamp.project.test.service.PhotoService;
 import bitcamp.project.test.service.TeamLeaderService;
 
 
 public class App {        
-	public String getGreeting() {
-		return "Hello world.";
-	}         
+  public String getGreeting() {
+    return "Hello world.";
+  }         
 
-	public static void main(String[] args) {
-		
+  public static void main(String[] args) {
 
-		System.out.println("** Made By Seung Been Oh **");
-		LessonService l = new LessonService();
-		l.list();
-		System.out.println("** Made By Seung Been Oh **");
 
-		PhotoService photoService = new PhotoService();
-		photoService.list();
+    System.out.println("** Made By Seung Been Oh **");
+    LessonService l = new LessonService();
+    l.list();
+    System.out.println("** Made By Seung Been Oh **");
 
-		System.out.println("------------");
-		TeamLeaderService s = new TeamLeaderService();
-		s.list();
-		System.out.println("------------");
+    PhotoService photoService = new PhotoService();
+    photoService.list();
 
-	}
+    System.out.println("------------");
+    TeamLeaderService s = new TeamLeaderService();
+    s.list();
+    System.out.println("------------");
+ 
+    MemberService memberService = new MemberService();
+    memberService.list();
+  }
 }
